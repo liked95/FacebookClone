bdcbc4859e0cd919b07c02947dbc3705df3090fc
liked <hoaison95.ftu@gmail.com>
2025-06-11 22:24:33 +0700

    Revert "Remove redundant userControllers"
    This reverts commit 78f0e8544a04fedd585d0edec94b188b8d1f3c18.

--- FacebookClone/Repositories/Implementations/UserRepository.cs
+++ FacebookClone/Repositories/Implementations/UserRepository.cs
@@ -18,5 +18,63 @@ namespace FacebookClone.Repositories.Implementations
         {
             return await context.Users.FirstOrDefaultAsync(x => x.Id == id);
         }
+
+        public async Task<User?> GetByUsernameAsync(string username)
+        {
+            return await context.Users.FirstOrDefaultAsync(x => x.Username == username);
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await context.Users.FirstOrDefaultAsync(x => x.Email == email);
+        }
+
+        public async Task<User?> CreateUserAsync(User user)
+        {
+            try
+            {
+                context.Users.Add(user);
+                await context.SaveChangesAsync();
+                return user;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public async Task<User?> UpdateUserAsync(User user)
+        {
+            try
+            {
+                context.Users.Update(user);
+                await context.SaveChangesAsync();
+                return user;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public async Task<bool> DeleteUserAsync(Guid id)
+        {
+            try
+            {
+                var user = await context.Users.FirstOrDefaultAsync(x => x.Id == id);
+                if (user == null)
+                {
+                    return false;
+                }
+
+                context.Users.Remove(user);
+                await context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
