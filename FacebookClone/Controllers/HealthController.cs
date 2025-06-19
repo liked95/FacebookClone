800d326158e6a4e7697dada87592e089da612379
liked <hoaison95.ftu@gmail.com>
2025-06-19 15:42:31 +0700

    Add health check

---
+++ FacebookClone/Controllers/HealthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace FacebookClone.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public HealthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // Basic health check
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("API is running.");
+        }
+    }
+}
